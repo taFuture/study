8ab41236fed8150f144247559d2adf169397638c
taFuture <3483988573@qq.com>
2023-06-27 08:04:45 +0000

    !1 合并 * refactor: 111 * docs: 网易云 * feat: 网易云 * feat: 0626的学习

---
+++ src/request/index.js
@@ -0,0 +1,7 @@
+// 所有的请求都定义在这个位置
+import axios from "axios";
+const http = axios.create({
+    baseURL:'https://netease-cloud-music-api-five-roan-88.vercel.app'
+})
+export const fetchPlaylist = (cat) => http.get('top/playlist',{params:{cat}});
+export const fetchPlaylistHot = () => http.get('/playlist/hot');
